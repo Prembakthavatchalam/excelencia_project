44c71b4161d59b55aaf871f5039ad9b7adaf4ecb
Prembakthavatchalam <125916214+Prembakthavatchalam@users.noreply.github.com>
2025-05-15 00:45:56 +0530

    Create variable.tf
    Create variable.tf

---
+++ module/variable.tf
@@ -0,0 +1,32 @@
+variable "sgname" {
+  type = string
+}
+#variable "vpc" {}
+variable "amiid" {
+  type = string
+}
+variable "keyname" {
+  type = string
+}
+variable "block1" {
+  type        = list(string)
+}
+variable "block2" {
+  type        = string
+}
+variable "block3" {
+  type        = string
+}
+variable "block4" {
+  type        = string
+}
+variable "block5" {
+  type        = string
+}
+#variable "vpc_name" {}
+variable "machinetype" {
+  type        = string
+}
+variable "vpc_name" {
+  type        = string
+}
